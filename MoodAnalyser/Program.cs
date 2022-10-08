03f048052dd68d1e62d9d5c8955f772e3a0f1809
kavyabs11 <kavyasomshekar11@gmail.com>
2022-10-07 21:30:44 -0700

    Analysing mood from the given message

---
+++ MoodAnalyser/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace MoodAnalyser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to MoodAnalyser MSTesting");
+            Console.WriteLine("Enter your mood to check");
+            string mood = Console.ReadLine();
+            MoodAnalyser obj = new MoodAnalyser();
+            obj.analyseMood(mood);
+          
+        }
+    }
+}
