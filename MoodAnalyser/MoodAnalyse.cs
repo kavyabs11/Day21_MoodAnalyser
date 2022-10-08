a576a8c55c62c65021c2a86561ebc2edfe781d49
kavyabs11 <kavyasomshekar11@gmail.com>
2022-10-07 22:01:37 -0700

    Analysing mood from the given message

--- MoodAnalyser/MoodAnalyse.cs
+++ MoodAnalyser/MoodAnalyse.cs
@@ -9,13 +9,22 @@ namespace MoodAnalyser
    
         public class MoodAnalyser
         {
-        public string analyseMood(string message)
+        private string message;
+        public MoodAnalyser() //default constructor
         {
-            if (message.ToUpper().Contains("SAD"))
+        }
+        public MoodAnalyser(string message) //parameterized constructor
+        {
+            this.message = message;
+        }
+        public string analyseMood()
+        {
+            if (this.message.Contains("sad"))
                 return "SAD";
             else
                 return "HAPPY";
         }
-        }
     }
+        }
+    
 
